b2435c5a5a7cc86436c839558ae7ce2837f48e4e
ajx-luck <122300543@qq.com>
2018-05-20 05:58:41 +0800

    关键词防污染

---
+++ src/main/java/com/breast/oil/excep/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.breast.oil.excep;
+
+import org.apache.catalina.connector.ClientAbortException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ClientAbortException.class)
+    @ResponseBody
+    public void connectionAbort(final ClientAbortException e) {
+        // Tomcat-specific exception when existing request is aborted by client
+
+        return ;
+    }
+}
