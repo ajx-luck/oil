613c2e563daf482f066db6a6d7dd021086d7e155
ajx-luck <122300543@qq.com>
2018-06-13 14:28:10 +0800

    hu7修改

--- src/main/java/com/breast/oil/web/WebController.java
+++ src/main/java/com/breast/oil/web/WebController.java
@@ -271,7 +271,7 @@ public class WebController {
                             return "redirect:/hu6.html";
                         }else{
                             setInfo(map, request,URL_3, "fxb", city,provice, response);
-                            return "redirect:/hu6.html";
+                            return "redirect:/fxxg.html";
                         }
 
                     }else {
@@ -284,7 +284,7 @@ public class WebController {
                     }
                 }else{
                     setInfo(map, request, URL_3,"fxd", city,provice, response);
-                    return "redirect:/hu6.html";
+                    return "redirect:/fxxg.html";
 
                 }
 
